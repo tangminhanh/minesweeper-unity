02fee767c4d8fec1937d7ee6dcd8bbcfb78a7a52
Lynn <57570218+tangminhanh@users.noreply.github.com>
2023-09-10 23:14:43 +0800

    Update Tile.cs

--- Scripts/Tile.cs
+++ Scripts/Tile.cs
@@ -4,18 +4,17 @@ using UnityEngine;
 public class Tile : MonoBehaviour
 {
     [Header("Tile Sprites")]
+    [SerializeField] private List<Sprite> revealTiles; //X and ?
     [SerializeField] private Sprite unclickedTile;
-    [SerializeField] private Sprite flaggedTile;
-    [SerializeField] private List<Sprite> clickedTiles;
-    [SerializeField] private Sprite mineTile;
-    [SerializeField] private Sprite mineWrongTile;
-    [SerializeField] private Sprite mineHitTile;
+    [SerializeField] private Sprite clickedTiles; //empty
+    [SerializeField] private Sprite mineTile; //reveal after game end
+    [SerializeField] private Sprite mineHitTile; //reveal when clicked
 
     //[Header("GM set via code")]
-    //public GameManager gameManager;
+    public GameManagement gameManagement;
 
     private SpriteRenderer spriteRenderer;
-    public bool flagged = false;
+    //public bool flagged = false;
     public bool active = true;
     public bool isMine = false;
     public int mineCount = 0;
@@ -36,88 +35,58 @@ public class Tile : MonoBehaviour
             {
                 // If left click reveal the tile contents.
                 ClickedTile();
-            }
-            else if (Input.GetMouseButtonDown(1))
-            {
-                // If right click toggle flag on/off.
-                flagged = !flagged;
-                if (flagged)
-                {
-                    spriteRenderer.sprite = flaggedTile;
-                }
-                else
-                {
-                    spriteRenderer.sprite = unclickedTile;
-                }
+                gameManagement.RevealSurroundingTiles(this);
+
             }
         }
-        //else
-        //{
-        //    // If you're pressing both mouse buttons.
-        //    if (Input.GetMouseButton(0) && Input.GetMouseButton(1))
-        //    {
-        //        // Check for valid expansion.
-        //        gameManager.ExpandIfFlagged(this);
-        //    }
-        //}
     }
+
+    //public void SetTileSprite(Sprite sprite)
+    //{
+    //    spriteRenderer.sprite = sprite;
+    //}
 
     public void ClickedTile()
     {
         // Don't allow left clicks on flags.
-        if (active & !flagged)
+        if (active)
         {
             // Ensure it can no longer be pressed again.
             active = false;
             if (isMine)
             {
-                // Game over :(
+                // Game over :( sad
                 spriteRenderer.sprite = mineHitTile;
                 //gameManager.GameOver();
             }
             else
             {
-                // It was a safe click, set the correct sprite.
-                spriteRenderer.sprite = clickedTiles[mineCount];
-                //if (mineCount == 0)
-                //{
-                //    // Register that the click should expand out to the neighbours.
-                //    gameManager.ClickNeighbours(this);
-                //}
+                // It was a safe click, empty tile
+                spriteRenderer.sprite = clickedTiles;
                 //// Whenever we successfully make a change check for game over.
                 //gameManager.CheckGameOver();
             }
         }
     }
-} 
 
-//    // If this tile should be shown at game over, do so.
-//    public void ShowGameOverState()
-//    {
-//        if (active)
-//        {
-//            active = false;
-//            if (isMine & !flagged)
-//            {
-//                // If mine and not flagged show mine.
-//                spriteRenderer.sprite = mineTile;
-//            }
-//            else if (flagged & !isMine)
-//            {
-//                // If flagged incorrectly show crossthrough mine
-//                spriteRenderer.sprite = mineWrongTile;
-//            }
-//        }
-//    }
+    public void Reveal()
+    {
+        if (!isMine)
+        {
+            spriteRenderer.sprite = revealTiles[0];
+        }
+        else
+        {
+            spriteRenderer.sprite = revealTiles[1];
+        }
+    }
 
-//    // Helper function to flag remaning mines on game completion.
-//    public void SetFlaggedIfMine()
-//    {
-//        if (isMine)
-//        {
-//            flagged = true;
-//            spriteRenderer.sprite = flaggedTile;
-//        }
-//    }
+    public bool IsRevealed()
+    {
+        // Check if the tile is already revealed
+        return spriteRenderer.sprite != unclickedTile;
+    }
+}
 
-//
+
+
